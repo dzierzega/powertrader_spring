e6477fb812d2c1e8bcc2d909294a390d65bc16ae
piotrdzierzega-innovise <piotr.dzierzega@innovise-esm.com>
2016-01-12 10:39:25 +0100

    XSRF - resolved

--- src/main/java/com/powertrader/config/CorsFilter.java
+++ src/main/java/com/powertrader/config/CorsFilter.java
@@ -19,8 +19,8 @@ public class CorsFilter implements Filter {
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
         HttpServletResponse response = (HttpServletResponse) res;
         HttpServletRequest request = (HttpServletRequest) req;
-//        response.setHeader("Access-Control-Allow-Credentials","true");
-        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Credentials","true");
+        response.setHeader("Access-Control-Allow-Origin", "http://localhost:8383");
         response.setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE");
         response.setHeader("Access-Control-Allow-Headers", "x-requested-with, X-Auth-Token, Authorization,X-XSRF-TOKEN");
         response.setHeader("Access-Control-Expose-Headers","set-cookie, Set-Cookie, SET-COOKIE");
